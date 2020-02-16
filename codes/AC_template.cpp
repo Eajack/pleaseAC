58445aff8635483faa40b96856290a68b2918944
Eajack <eajacklr@gmail.com>
2020-02-16 20:23:55 +0800

    Create AC_template.cpp

---
+++ codes/AC_template.cpp
@@ -0,0 +1,48 @@
+/*
+Aurthor: Eajack Lau
+Updated date: 2020/2/16
+Source: https://github.com/Eajack/pleaseAC
+Function: leetcode等刷题的cpp模板
+*/
+//1- C library
+#include <cctype>
+#include <cmath>
+#include <climits>
+#include <cstddef>
+#include <cstdlib>
+//2- Contaniners
+#include <deque>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+//3- Input/Output
+#include <iostream>
+//4- Other
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+//leetcode代码
+class Solution
+{
+public:
+	int code(void){
+		/* code */
+		return 0;
+	}
+};
+
+//测试代码
+int main(void)
+{
+	/* code */
+	Solution S;
+	int a = S.code();
+	
+	return 0;
+}
